eeff6593c2ff9a1d3eedc4897faa6b28b2986987
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-02-24 14:15:57 +0500

    init

---
+++ make-app.mk
@@ -0,0 +1,13 @@
+USER = "$(shell id -u):$(shell id -g)"
+
+app:
+	docker-compose up
+
+app-build:
+	docker-compose build
+
+app-bash:
+	docker-compose run --user=$(USER) app bash
+
+app-setup: app-build
+	docker-compose run --user=$(USER) app bin/setup
