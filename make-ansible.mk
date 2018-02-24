2a49114a760b46790f9bcf5cb7bc3b55c473d0d0
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-02-24 20:25:24 +0500

    done with compose test

--- make-ansible.mk
+++ make-ansible.mk
@@ -3,5 +3,5 @@ ansible-vaults-encrypt:
 	ansible-vault encrypt ansible/group_vars/all/vault.yml
 
 ansible-vaults-decrypt:
-	ansible-vault decrypt ansible/development/group_vars/all/vault.yml
-	ansible-vault decrypt ansible/group_vars/all/vault.yml
+	ansible-vault decrypt ansible/development/group_vars/all/vault.yml --ask-vault-pass
+	ansible-vault decrypt ansible/group_vars/all/vault.yml --ask-vault-pass
