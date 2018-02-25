afe7c597608ea66d1eb2dbf3af1b9054afddada0
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-02-25 18:25:57 +0500

    step 36-39

--- make-production.mk
+++ make-production.mk
@@ -1,2 +1,5 @@
 production-setup:
 	ansible-playbook ansible/site.yml -i ansible/production -u ubuntu --ask-vault-pass -vv
+
+production-deploy:
+	ansible-playbook ansible/deploy.yml -i ansible/production -u ubuntu --ask-vault-pass -vv
