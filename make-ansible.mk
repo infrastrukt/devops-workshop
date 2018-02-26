a33235627c309b3575deb3d36fb94ca816b6a84f
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-02-26 05:29:39 +0500

    step 40-41

--- make-ansible.mk
+++ make-ansible.mk
@@ -3,8 +3,10 @@ ansible-deps-install:
 
 ansible-vaults-encrypt:
 	ansible-vault encrypt ansible/development/group_vars/all/vault.yml
+	ansible-vault encrypt ansible/production/group_vars/all/vault.yml
 	ansible-vault encrypt ansible/group_vars/all/vault.yml
 
 ansible-vaults-decrypt:
 	ansible-vault decrypt ansible/development/group_vars/all/vault.yml --ask-vault-pass
+	ansible-vault decrypt ansible/production/group_vars/all/vault.yml --ask-vault-pass
 	ansible-vault decrypt ansible/group_vars/all/vault.yml --ask-vault-pass
